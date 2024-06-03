3b672e684f2b4ca103d715803cb23dacddc98d8d
[GitHubName]~ <[GitHubEmail]>
2024-06-03 11:24:03 +0900

    kw

--- react/my-page/src/pages/ChartArea.jsx
+++ react/my-page/src/pages/ChartArea.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CharArea.css';
+import './ChartArea.css';
 
 
 const ChartArea = () => {
--- react/my-page/src/pages/Search.jsx
+++ react/my-page/src/pages/Search.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Search.css'; // Importing the CSS file
 import '../fonts.css'; // Import the new CSS file with the font-face rule
-import './About.jsx';
-import './ChartArea.jsx';
-
-
+import ChartArea from './ChartArea'; // ChartArea 컴포넌트 불러오기
 
 function Search() {
   return (
@@ -19,9 +16,10 @@ function Search() {
           <button className="input-button">입력</button>
         </div>
       </div>
-     <ChartArea/>
+      <ChartArea /> {/* ChartArea 컴포넌트 사용 */}
     </div>
   );
 }
 
 export default Search;
+
