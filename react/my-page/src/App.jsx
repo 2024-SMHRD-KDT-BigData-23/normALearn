cff0622029e117ac4b90978272edd13e3923dbda
zzoorong <steam0629@gmail.com>
2024-06-05 09:06:48 +0900

    ar

--- react/my-page/src/App.jsx
+++ react/my-page/src/App.jsx
@@ -1,30 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-// css 관련
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; // Import the CSS
-import './fonts.css'; // Import the new CSS file with the font-face rule
-
-// 변수 관련
+import './App.css';
+import './fonts.css';
 import Sidedown, { RenderList } from "./pages/Sidedown";
-
-// 페이지 관련
 import Search from "./pages/Search";
-import { useUserInfoData, useLoginInfo } from "./pages/Log"; 
+import { useUserInfoData, useLoginInfo } from "./pages/Log";
 import Input2 from "./pages/Input2";
 import Counter from "./pages/Counter";
 
-
-
 function App() {
-    // Sidedown.jsx에서 불러오는 기능들
     const {
         orderedData,
         fixedList,
         handleCheckboxChange
-    } = Sidedown(); // 커스텀 훅
+    } = Sidedown();
 
-    // Log에서 커스텀 훅 사용
     const {
         userId,
         userPw,
@@ -35,20 +26,25 @@ function App() {
         setIsLoggedIn
     } = useLoginInfo();
 
-    // Log에서 사용자 정보 관련 훅 사용
     const {
-        userName,
         companyName,
         handleLogout,
         setUserInfoAfterLogin,
     } = useUserInfoData(setIsLoggedIn);
 
+    useEffect(() => {
+        const savedUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+        if (savedUserInfo) {
+            setIsLoggedIn(true);
+            setUserInfoAfterLogin(savedUserInfo);
+        }
+    }, [setIsLoggedIn, setUserInfoAfterLogin]);
+
     return (
         <div className="App">
             <nav className="main-menu">
-                {/* 사이트 탑 관련 기능 정의 */}
                 <div className="side-top">
-                <ul className="user-info">
+                    <ul className="user-info">
                         {!isLoggedIn ? (
                             <li className="login-fields">
                                 <input 
@@ -68,7 +64,7 @@ function App() {
                         ) : (
                             <>
                                 <li className="user-name">
-                                    <p>{userId} ({companyName})</p><i className="fa fa-angle-right"></i>
+                                    <p>{companyName}</p><i className="fa fa-angle-right"></i>
                                 </li>
                                 <li className="user-actions">
                                     <p onClick={handleLogout}>logout</p>
@@ -85,12 +81,6 @@ function App() {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/about">
-                                <i className="fa fa-bar-chart"></i>
-                                <span className="nav-text">시각화 페이지</span>
-                            </Link>
-                        </li>
-                        <li>
                             <Link to="/input2">
                                 <i className="fa fa-bookmark"></i>
                                 <span className="nav-text">북마크 페이지</span>
@@ -104,20 +94,13 @@ function App() {
                         </li>
                     </ul>
                 </div>
-
-                {/* side-down 관련 기능 정의 */}
-               
-                    <div className="scrollbar" id="style-1">
-                        <RenderList data={orderedData} fixedList={fixedList} handleCheckboxChange={handleCheckboxChange} />
-                    </div>
-                
+                <div className="scrollbar" id="style-1">
+                    <RenderList data={orderedData} fixedList={fixedList} handleCheckboxChange={handleCheckboxChange} />
+                </div>
             </nav>
-
-            {/* 메인 페이지 변경 부분 링크 기능 */}
             <div className="content-container">
                 <Routes>
                     <Route path="/" element={<Search />} />
-                   
                     <Route path="/input2" element={<Input2 />} />
                     <Route path="/counter" element={<Counter />} />
                 </Routes>
