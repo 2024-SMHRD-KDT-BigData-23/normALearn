7a9583f13a360dc29f6a2a6bcc62bf75fa542118
Your Name <you@example.com>
2024-06-13 15:34:53 +0900

    kwsdo

--- react/my-page/src/pages/Chart.jsx
+++ react/my-page/src/pages/Chart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Chart.css';
-import DoughnutChart from './Chartsup';
+import DoughnutChart from './DoughnutChart';
 
 const Chart = ({ vsData }) => {
   // 데이터를 수신하고 콘솔에 출력
