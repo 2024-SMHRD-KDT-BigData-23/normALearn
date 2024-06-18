84821a5c29bacef68f683de95cc5d87d25556592
주우건 <155132203+Joowoogun@users.noreply.github.com>
2024-06-18 15:07:59 +0900

    woogunaasdasd

--- SpringBoot/projectA/src/main/java/com/projectA/Controller/BackController.java
+++ SpringBoot/projectA/src/main/java/com/projectA/Controller/BackController.java
@@ -50,17 +50,13 @@ public class BackController {
 				work = ResultInfo.getWork();
 			}
 		} catch (Exception e) {
-			System.out.println("work가 null값입니다.");
+			System.out.println("Front에서 무슨작업을 하는지 같이 보내야합니다.");
 		}
-
 		if (work.equals("myPage")) { // 프론트에서 work 파라미터에 myPage 라고 보내면 유저아이디에 맞는 자료 마이페이지 리스트만 불러온다.
 			List<Al_resultVO> data = result.getMypageList(ResultInfo);
-			System.out.println("BookMark출력");
 			return ResponseEntity.ok(data);
 		} else {
-			// DB의 al_result 에서 가져온 값들을 리스트 data 에 담아서 리엑트로 보낸다.
 			List<Al_resultVO> data = result.getResultList(ResultInfo);
-			System.out.println("전챗값 출력");
 			return ResponseEntity.ok(data);
 		}
 
@@ -69,20 +65,12 @@ public class BackController {
 	@PostMapping("/sendListResult") // 리스트 변경사항 있을 떄 작동하는 메소드 (반환할게 없을때 사용)
 	public void sendListResult(@RequestBody Al_resultVO ResultInfo) {
 		String work = ResultInfo.getWork();
-		// 무슨 작업을 하는지 확인 프린트
-		System.out.println(ResultInfo.getOutputIdx());
-		System.out.println(work);
-		// if 문 사용 무슨 작업하는지 캐치
 		if (work.equals("ChangeNumber")) {
-			// 만약 react에서 ChangeNumber라는 문장을 work에 담아서 보냈을때 실행되는 매퍼 ListInfo에서 oldNumber,
-			// newNumber 가져와 쿼리문 작동
 			result.changeResultNumber(ResultInfo);
 		} else if (work.equals("ChangeCheckBox")) {
-			// sql ListInfo 내부의 favorite 값과 number 의 값을 활용해 변경
 			result.changeResultCheckBox(ResultInfo);
 		} else if (work.equals("ChangeMypage")) {
 			result.mypageCheck(ResultInfo);
-			System.out.println("마이페이지 변경완료");
 		} else {
 			System.out.println("오류");
 		}
@@ -96,56 +84,40 @@ public class BackController {
 
 	@PostMapping("/sendListOutput") //
 	public void sendListOutput(@RequestBody Al_outputVO OutputInfo) {
-		
 		String work = "없음";
 		try {
 			work = OutputInfo.getWork();
-			System.out.println("작업내용 : "+work);
-			// 무슨 작업을 하는지 확인 프린트
-		} catch (Exception e) {}
-		if(work.equals("updateProductName")) {
+		} catch (Exception e) {
+		}
+		if (work.equals("updateProductName")) {
 			output.updateProductName(OutputInfo);
 		}
-		
-		
-		
 
 	}
 
 	@PostMapping("/sendSearchData") // 조성을 입력해 검색하는 기능
 	public ResponseEntity<List<Al_outputVO>> sendSearchData(@RequestBody Al_inputVO inputData) {
-		// 검색자료를 토대로 output 자료 출력
-		// 입력받은 데이터를 콘솔에 출력
-		System.out.println("검색 데이터" + inputData);
 		List<Al_outputVO> SearchResult = input.insertInputAndUseIdxSearchOutput(inputData);
-		// 일단 DB에서 가져와서 바로 출력하는 기능임 -> 머신러닝 구현해서 코드 추가할것
 		System.out.println(SearchResult.get(0).getInputIdx());
 		return ResponseEntity.ok(SearchResult);
 	}
 
-	@PostMapping("/clickListData")
-    public ResponseEntity<List<Al_outputVO>> clickListData(@RequestBody Al_resultVO data){
-        System.out.println(data.getOutputIdx());
-        System.out.println(data.getUserId());
-        List<Al_outputVO> ClickData = result.ClickListSerch(data);
-        try {
-            System.out.println(ClickData.get(0));
-        } catch (Exception e) {
-            System.out.println("DataMissing");
-        } 
-        return ResponseEntity.ok(ClickData);
-    }
+	@PostMapping("/clickListData") // 좌측 검색기록 클릭시 데이터 전송 -> 차트에 바로 표시됨
+	public ResponseEntity<List<Al_outputVO>> clickListData(@RequestBody Al_resultVO data) {
+		System.out.println(data.getOutputIdx());
+		System.out.println(data.getUserId());
+		List<Al_outputVO> ClickData = result.ClickListSerch(data);
+		try {
+			System.out.println(ClickData.get(0));
+		} catch (Exception e) {
+			System.out.println("DataMissing");
+		}
+		return ResponseEntity.ok(ClickData);
+	}
 
 	@PostMapping("/ChangePw") // 비밀번호 변경 userId, userPw, newPw 보내면됨
 	public ResponseEntity<Map<String, Object>> ChangePw(@RequestBody Al_userVO data) {
 		Map<String, Object> responseBody = new HashMap<>();
-		try {
-			System.out.println(data.getUserId());
-			System.out.println(data.getNewPw());
-			System.out.println(data.getUserPw());
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
 		int row = user.ChangePw(data);
 		if (row == 1) {
 			responseBody.put("message", "ok");
@@ -154,35 +126,29 @@ public class BackController {
 			responseBody.put("message", "fail");
 			return ResponseEntity.ok(responseBody);
 		}
-
 	}
-	
+
 	@PostMapping("/board")
-	public ResponseEntity<List<Al_boardVO>> board(@RequestBody Al_boardVO data){
+	public ResponseEntity<List<Al_boardVO>> board(@RequestBody Al_boardVO data) {
 		String work = "없음";
 		try {
 			work = data.getWork();
-			System.out.println("작업내용 = "+work);
-		} catch (Exception e) {}
-		if(work.equals("write")) {
+		} catch (Exception e) {
+		}
+		if (work.equals("write")) {
 			board.writeBoard(data);
-			System.out.println("작성완료");			
-		}else if(work.equals("edit")) {
-			// 게시물 수정시 제목 수정안해도 같이 보내야 합니다. 밸류값 가져오는 방식으로. sql update 문 한번에 바꾸는 거라..
+		} else if (work.equals("edit")) {
 			board.editBoard(data);
-			System.out.println("수정완료");
-		}else if(work.equals("Status")) {
+		} else if (work.equals("Status")) {
 			board.editProgress(data);
-			System.out.println("상태변경");
-		}else if(work.equals("delete")) {
+		} else if (work.equals("delete")) {
 			board.deleteBoard(data);
-			System.out.println("삭제완료");
-		}else {
-			if(data.getUserId().equals("admin")) { // 만약 로그인한 계정이 "admin" 이라면 아이디 상관없이 전부 가져옴 
-				List<Al_boardVO> responseBody =board.getAllBoardList(data);
+		} else {
+			if (data.getUserId().equals("admin")) { // 만약 로그인한 계정이 "admin" 이라면 아이디 상관없이 전부 가져옴
+				List<Al_boardVO> responseBody = board.getAllBoardList(data);
 				return ResponseEntity.ok(responseBody);
-			}else {
-				List<Al_boardVO> responseBody =board.getBoardList(data);
+			} else {
+				List<Al_boardVO> responseBody = board.getBoardList(data);
 				return ResponseEntity.ok(responseBody);
 			}
 		}
@@ -192,25 +158,21 @@ public class BackController {
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, Object>> login(@RequestBody Al_userVO loginData, HttpSession session,
 			HttpServletResponse response) {
-		Map<String, Object> responseBody = new HashMap<>();
-		Al_userVO result = user.login(loginData);
-		if (result != null) { // 로그인 성공 시
-			System.out.println("로그인 성공");
+			Map<String, Object> responseBody = new HashMap<>();
+			Al_userVO result = user.login(loginData);
+			if (result != null) { 
 
-			// 세션에 사용자 정보 저장
 			Al_userVO user = new Al_userVO();
 			user.setCompanyName(result.getCompanyName());
 			user.setUserId(result.getUserId());
 			session.setAttribute("userId", user);
 
-			// 쿠키 설정
 			Cookie cookie = new Cookie("Alsession", session.getId());
 			cookie.setHttpOnly(true);
 			cookie.setPath("/");
 			cookie.setMaxAge(30);
 			response.addCookie(cookie);
 
-			// 응답에 최소한의 사용자 정보 포함 + 회사이름
 			responseBody.put("message", "로그인 성공");
 			responseBody.put("userId", user.getUserId());
 			responseBody.put("companyName", user.getCompanyName());
